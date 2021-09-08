261d428d17e5908e4d7fe4774cf638c0740a82ae
uzairzahari <uzairzahari@yahoo.com>
2021-09-08 14:36:25 +0800

    added SASS - css preprocessor

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+export default defineConfig ({
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: { 
+         // example : additionalData: `@import "./src/design/styles/variables";`
+         // dont need include file extend .scss
+         additionalData: `@import "./src/styles/variables";` 
+     },
+    },
+  },
+});
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
